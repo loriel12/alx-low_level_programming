bd24cd824022231f1a68dd396367df0b805a9159
“Your <lalaberry35@gmail.com>
2023-03-21 19:44:44 -0700

    10. A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,21 +1,19 @@
 #include "main.h"
+
 /**
- * reverse_array - reverse array of integers
- * @a: array
- * @n: number of elements of array
- *
- * Return: void
- */
+*  reverse_array - function that reverse content of array
+*  @a: array
+*  @n: number of elements
+*/
+
 void reverse_array(int *a, int n)
 {
-	int i;
-	int t;
+	int tmp, index;
 
-	for (i = 0; i < n--; i++)
+	for (index = n - 1; index > n / 2; index--)
 	{
-		t = a[i];
-		a[i] = a[n];
-		a[n] = t;
+	tmp = a[n - 1 - index];
+	a[n - 1 - index] = a[index];
+	a[index] = tmp;
 	}
 }
-
